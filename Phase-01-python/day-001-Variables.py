3bf04d279e55e397722ec7dc15b2d2c3f9f0d52f
galgallo <galgallogodana67@gmail.com>
2026-05-03 11:55:47 +0300

    Day 1

---
+++ Phase-01-python/day-001-Variables.py
@@ -0,0 +1,17 @@
+#string
+Name = 'Galgallo'
+School = 'Havard University'
+Passion = 'Ai-Engineer'
+
+#integer
+Age = 19
+
+#float
+Net_worth = 27,000.67
+
+#boolean
+he_is_disciplined = True
+
+
+print (f'{Name} is {Age} year old, '
+         f'Studying at {School} and he is so passionate about {Passion}')
