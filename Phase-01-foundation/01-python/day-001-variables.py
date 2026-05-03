eea2c61f960a8fd8376998cd4e95fca386d0bde1
galgallo <galgallogodana67@gmail.com>
2026-05-03 18:16:19 +0300

    Day one

---
+++ Phase-01-foundation/01-python/day-001-variables.py
@@ -0,0 +1,34 @@
+#This is May 03th 2026
+
+#string
+#       in python they are texts enclosed in quotes, Example:
+name = 'Galgallo'
+school = 'Havard University'
+passion = 'Ai'
+location = 'Africa'
+
+print(f'{name} studies at {school} and he has passion in {passion}')
+
+#integers
+#       an integer should not be in quotes, only strings are stored in quotes.
+age = 19
+quantity = 10
+years_of_experience = 6
+
+print(f'he is {age} year old')
+print(f'he has {years_of_experience} years of experience in procastination')
+
+#float
+#       these are decimal numbers
+price = 900.00
+gpa = 3.9
+distance = 5.5
+
+print(f'he got a gpa of {gpa}')
+
+#boolean
+#       this have false or true
+
+he_study_abroad = True
+he_is_lying = False
+he_likes_to_study_abroad = True
