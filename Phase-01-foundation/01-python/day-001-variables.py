cf5579843d7123d502d10337e084d85ca6eb3423
galgallo <galgallogodana67@gmail.com>
2026-05-03 22:36:22 +0300

    Day One down✔️

--- Phase-01-foundation/01-python/day-001-variables.py
+++ Phase-01-foundation/01-python/day-001-variables.py
@@ -17,7 +17,7 @@ years_of_experience = 6
 
 print(f'he is {age} year old')
 print(f'he has {years_of_experience} years of experience in procastination')
-
+print(quantity)
 #float
 #       these are decimal numbers
 price = 900.00
@@ -32,3 +32,41 @@ print(f'he got a gpa of {gpa}')
 he_study_abroad = True
 he_is_lying = True
 he_likes_to_study_abroad = True
+
+
+#Typecasting - is process of converting a variable from one data type to another.
+
+#integer to string
+students = 50
+teachers = 15
+
+students = str(students)
+teachers = str(teachers)
+
+#to check the type of a variable
+print(type(students))
+print(type(teachers))
+
+
+#float to integer
+profit = 5000.14
+amount = 1500.04
+
+profit = int(profit)
+amount = int(amount)
+
+#to check the type of a variable
+print(type(profit))
+print(type(amount))
+
+
+#integer to float
+salary = 500
+expenses = 150
+
+salary = float(salary)
+expenses = float(expenses)
+
+#to check the type of a variable
+print(type(salary))
+print(type(expenses))
