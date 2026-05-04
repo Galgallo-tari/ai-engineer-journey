a13513dc7672651555757545f3d169723f411013
galgallo <galgallogodana67@gmail.com>
2026-05-05 00:45:54 +0300

    Day two down✔️

--- Phase-01-foundation/01-python/day-002-data-types.py
+++ Phase-01-foundation/01-python/day-002-data-types.py
@@ -1,1 +1,50 @@
 #Data types
+
+#string
+name = 'Galgallo'
+
+print(type(name))
+
+#integer
+salary = 50000
+
+print(type(salary))
+
+#float
+weight = 56.7
+
+print(type(weight))
+
+#boolean
+is_raining = True
+
+print(type(is_raining))
+
+#set
+my_set = {4.5, 'hello', 700}
+
+print(type(my_set))
+
+#list
+my_list = [3.78, 'galgallo', 'banana']
+
+print(type(my_list))
+
+#tuple
+my_tuple = ('mango', 'apple', 500)
+
+print(type(my_tuple))
+
+#dictionary
+my_details = {'Name':'Galgallo',
+              'School':'Havard',
+              'networth':'50,000$'
+}
+
+print(type(my_details))
+
+#range
+my_range_var = range(5)
+print('Range:', my_range_var) # Range: range(0, 5)
+
+print(type(my_range_var))
