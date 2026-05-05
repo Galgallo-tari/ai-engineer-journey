bdaa2825915828c3c76269c47d4ad3fa21b3cf6f
galgallo <galgallogodana67@gmail.com>
2026-05-06 00:13:17 +0300

    Side quizes 001

---
+++ Phase-01-foundation/01-python/day-004-lists.py
@@ -0,0 +1,12 @@
+name = 'Galgallo'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 19
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score))
