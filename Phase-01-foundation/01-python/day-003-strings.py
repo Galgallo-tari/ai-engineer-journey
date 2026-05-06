cd685c7f846240bc14140bf42ab6c2be3f688aaf
galgallo <galgallocoder@gmail.com>
2026-05-06 07:30:55 +0300

    May 6th 2026, quiztry2

--- Phase-01-foundation/01-python/day-003-strings.py
+++ Phase-01-foundation/01-python/day-003-strings.py
@@ -56,12 +56,84 @@ school = 'Glory Primary'
 
 print(f'{teacher} is {position} of {school}')
 
-#String slicing lets you extract a portion of a string or work with only a specific part of it
+# String slicing lets you extract a portion of a string or work with only a specific part of it
 # [START:STOP:STEP]
 
 brand = 'Mitsubishi'
 print(brand[2:10:2])       #tuih
 print(brand[0::1])    #Mitsubishi
+
+
+#Strings Methods
+# 1. upper()
+my_str = 'books'
+my_upper_str = my_str.upper()
+print(my_upper_str)         #BOOKS
+
+# 2. lower()
+str = 'ALCHEMIST'
+my_lower_str = str.lower()
+print(my_lower_str)         #alchemist
+
+# 3. title
+book = 'the miracle morning'
+my_book = book.title()
+print(my_book)          #The Miracle Morning
+
+# 4. replace(old, new)
+perfume = 'Now'
+my_perfume = perfume.replace('Now', 'Farah')
+print(my_perfume)       #Farah
+
+# 5. strip()
+grade = ' seven    '
+my_grade = grade.strip()
+print(my_grade)             #seven.... it removes leads and white spaces
+
+# capitalize
+books = 'the miracles morning'
+my_books = books.capitalize()
+print(my_books)         #The miracles morning
+
+# split(separator)
+greeting = 'Hello World'
+my_greeting = greeting.split()
+print(my_greeting)          #['Hello', 'World']
+
+# join(iterable)
+my_list = ['hi', 'hello']
+my_full_list = ' '.join(my_list)
+print(my_full_list)             #hi hello
+
+#startswith(prefix)
+gadgets = 'phone, laptop'
+start_with_laptop = gadgets.startswith('laptop')
+print(start_with_laptop)            #False
+
+#endswith(suffix)
+gadgets = 'phone, laptop'
+end_with_laptop = gadgets.endswith('laptop')
+print(end_with_laptop)          #True
+
+#find(substring)
+my_str = 'Hello World'
+my_find_str = my_str.find('World')
+print(my_find_str)          #6
+
+#count(substring)
+my_str = 'GoodMorning World'
+my_count_str = my_str.count('o')
+print(my_count_str)         #4
+
+#isupper
+greeting = 'Good Morning'
+my_greeting = greeting.isupper()
+print(my_greeting)          #False
+
+#islower()
+greeting = 'good morning'
+my_greeting = greeting.islower()
+print(my_greeting)          #True
 
 
 
