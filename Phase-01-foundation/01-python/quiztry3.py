fc5e92d7ade5b6bbd8855412b89bd8f0bfe3c0b8
galgallo <galgallocoder@gmail.com>
2026-05-07 07:39:46 +0300

    May 7th 2026, quiztry3

---
+++ Phase-01-foundation/01-python/quiztry3.py
@@ -0,0 +1,23 @@
+running_total = 0
+
+friends_num = 2
+
+coffee = 34.89
+pizza = 52.34
+cake = 29.39
+take_away = 64.27
+
+running_total += coffee + pizza + cake + take_away
+print('Total bill so far:$', running_total)
+
+tip = running_total * 0.25
+print('Tip amount:$', tip)
+
+running_total += tip
+print('Total with tip:$', running_total)
+
+final_bill = running_total / friends_num
+print('Bill per person:$', final_bill)
+
+each_pays = round(final_bill, 2)
+print(f'Each person pays: ${each_pays}')
