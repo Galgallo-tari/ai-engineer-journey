01c0dac264f6e61d43b5e01bb2e1726833b6b51e
Galgallo <galgallocoder@gmail.com>
2026-05-23 22:08:38 +0300

    19th May - Build an RPG Character

--- Phase-01-foundation/01-python/quiztry9.py
+++ Phase-01-foundation/01-python/quiztry9.py
@@ -5,6 +5,7 @@ full_dot = '●'
 empty_dot = '○'
 
 
+
 def create_character(name, strength, intelligence, charisma):
     if not isinstance(name, str):
         return 'The character name should be a string'
